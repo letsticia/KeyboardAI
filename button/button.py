c78396dc2fa713776a0a7f2653ef22b385bce9a9
letsticia <leti.morais48@gmail.com>
2024-05-01 18:45:56 -0300

    added Button class

---
+++ button/button.py
@@ -0,0 +1,30 @@
+import cv2
+
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.text = text
+        self.size = size
+        
+    def draw(self, frame):
+        x, y = self.pos
+        w, h = self.size
+        
+        cv2.rectangle(frame, self.pos, (x+w, y+h), RETANGLE_COLOR, cv2.FILLED)
+        cv2.putText(frame, self.text, (x+20, y+65), 
+                    cv2.FONT_HERSHEY_PLAIN, 4, TEXT_COLOR, 4)
+ 
+        return frame
+    
+buttonList = []
+
+RETANGLE_COLOR = (255, 0, 255)
+TEXT_COLOR = (255, 255, 255)
+
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+
+for row in range(len(keys)):
+    for numkey, key in enumerate(keys[row]):
+        buttonList.append(Button([100 + numkey*100, 100 + row*100], key))
