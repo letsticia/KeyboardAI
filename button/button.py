a337c72c48a5d6af12e317d270397055175f5bd6
letsticia <leti.morais48@gmail.com>
2024-05-01 20:41:03 -0300

    feat: added isOver and click methods

--- button/button.py
+++ button/button.py
@@ -6,6 +6,7 @@ class Button():
         self.text = text
         self.size = size
         
+        
     def draw(self, frame):
         x, y = self.pos
         w, h = self.size
@@ -16,10 +17,35 @@ class Button():
  
         return frame
     
+    def isOver(self, lmList, frame):
+        x, y = self.pos
+        w, h = self.size
+        
+        if x < lmList[8][0] < x+w and y < lmList[8][1] < y+h:
+            cv2.rectangle(frame, self.pos, (x+w, y+h), OVER_COLOR, cv2.FILLED)
+            cv2.putText(frame, self.text, (x+20, y+65), 
+                        cv2.FONT_HERSHEY_PLAIN, 4, TEXT_COLOR, 4)
+            return True
+    
+    def click(self, lenght, frame, final_text):
+        x, y = self.pos
+        w, h = self.size
+        
+        if lenght < 30:         
+            cv2.rectangle(frame, self.pos, (x+w, y+h), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, self.text, (x+20, y+65), 
+                        cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+            
+            final_text += self.text
+            return final_text
+      
+            
+    
 buttonList = []
 
 RETANGLE_COLOR = (255, 0, 255)
 TEXT_COLOR = (255, 255, 255)
+OVER_COLOR = (175, 0, 175)
 
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 from button import buttonList
+import time
 
+final_text = ""
+
+last_click_time = 0
+cooldown = 0.7
 
 video_capture = cv2.VideoCapture(0)
 video_capture.set(3, 1280)
@@ -9,16 +14,39 @@ video_capture.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
 
+
 while True:
     sucess, frame = video_capture.read()
     
     frame = cv2.flip(frame, 1)
     
     hands, frame = detector.findHands(frame)
-    
+        
     for button in buttonList:
         frame = button.draw(frame)
-    
+        
+    if hands:
+        lmList = hands[0]['lmList']
+        bbox = hands[0]['bbox']
+        
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+            
+            over = button.isOver(lmList, frame)
+            
+            if over:
+                lenght, _, _ = detector.findDistance(lmList[8][:2], lmList[4][:2], frame)
+                click = button.click(lenght, frame, final_text)
+                
+                if click and (time.time() - last_click_time) > cooldown:
+                    final_text = click
+                    last_click_time = time.time()
+                
+                
+                cv2.rectangle(frame, (50, 420), (700, 550), (175, 0, 175), cv2.FILLED)
+                cv2.putText(frame, final_text, (68, 525), 
+                            cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 4)
     
     if frame is None:
         continue
